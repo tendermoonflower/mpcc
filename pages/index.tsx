a84b1328b1ad6e83254bda30e2a0262232cab863
tendermoonflower <155327060+tendermoonflower@users.noreply.github.com>
2026-02-23 10:07:48 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -154,7 +154,7 @@ function Headshot() {
         <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-rose-100 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
           <Image
             className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-            src="/AI_headshot.png"
+            src="/2025headshot.JPG"
             height={400}
             width={400}
             alt=""

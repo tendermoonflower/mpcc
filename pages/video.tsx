386ecc4d0b3cb42bd1c7ef63f263ae2b6310048e
tendermoonflower <155327060+tendermoonflower@users.noreply.github.com>
2026-06-26 16:01:05 -0400

    Update video.tsx

--- pages/video.tsx
+++ pages/video.tsx
@@ -53,11 +53,14 @@ export default function VideoPage() {
             </h2>
 
             <p className="text-lg leading-8 text-blue-950/90">
-              The purpose of writing practice:
+              The purpose of brainstorming:
               
-writing is, above all, a practice in reflection. the purpose of writing practice is to follow the breadcrumbs of your thoughts back to your true self. in allowing your thoughts to arise spontaneously, and without censoring them, you will learn things about yourself that you never knew, or things you thought you forgot. the purpose of writing practice is not even to produce a common app essay. we're not even there yet. we're still trying to figure out who we are, and why we are that way. so don't feel like there is any end-goal. the only goal of writing practice is to follow our thoughts wherever they lead. having no destination, i am never lost. 
+brainstorming is, above all, a practice in reflection. the purpose of brainstorming from the side is to follow the breadcrumbs of your thoughts back to your true feelings and motivations. 
+              in allowing your thoughts to arise spontaneously, and without censoring them, you will learn things about yourself that you never knew, or things you thought you forgot. 
+              the purpose of brainstorming from the side is not even to produce a common app essay. we're not even there yet. we're still trying to figure out who we are, and why we are that way. 
+              so don't feel like there is any end-goal. the only goal of writing practice is to follow our thoughts wherever they lead. having no destination, i am never lost. 
 
-Rules for writing practice:
+Rules for brainstorming from the side:
 1. write by hand. there are a lot of reasons why it is more effective to do this type of writing by hand, and i'm happy to explain next time we meet. for now, just trust that there is something secret and special that arises when we write by hand instead of type on a computer.
 
 2. commit to staying with each prompt for at least 30 minutes, even if you stray off path or write in circles. in fact, if you do stray off path, that's how you know you're doing it right. write for longer than 30 minutes if you're really feeling it. 
@@ -72,7 +75,10 @@ no, "i'm not a good writer,"
 no "i'm too boring to have anything to say," 
 no "this experience doesn't even matter."
 
-none of these statements are true. you're going to hear the voice in your head repeat these phrases to you. this is "monkey mind." it's just the voice in your head that chatters on, mostly with criticism or, "im bored, lets go eat." sometimes when you hear the voice it can be helpful to envision the voice as a person. what do they look like? why are they so critical? why are they trying to take you away from your work? are they worried you're going to stumble upon something important and how dare you feel important?      and then, you can imagine telling them to keep it down while you keep on writing.
+none of these statements are true. you're going to hear the voice in your head repeat these phrases to you. this is "monkey mind." it's just the voice in your head that chatters on, mostly with criticism or, "im bored, lets go eat." 
+              sometimes when you hear the voice it can be helpful to envision the voice as a person. what do they look like? why are they so critical? why are they trying to take you away from your work? 
+              are they worried you're going to stumble upon something important and how dare you feel important?
+              and then, you can imagine telling them to keep it down while you keep on writing.
 
             </p>
           </section>

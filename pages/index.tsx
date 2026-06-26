df99118de6a9281d20e5838a58ec80be0b1f6bf9
tendermoonflower <155327060+tendermoonflower@users.noreply.github.com>
2026-06-26 15:35:42 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,7 @@ function Hero() {
     { name: "services", href: "#services" },
     { name: "testimonials", href: "#testimonials" },
     { name: "contact me", href: "#contact" },
+    { name: "tips for students", href: "/video" },
   ];
 
   return (

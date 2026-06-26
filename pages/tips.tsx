1362a21f9a84b37fb6ad0b76bf1e4799a85216af
tendermoonflower <155327060+tendermoonflower@users.noreply.github.com>
2026-06-26 17:28:45 -0400

    Update tips.tsx

--- pages/tips.tsx
+++ pages/tips.tsx
@@ -59,7 +59,7 @@ export default function VideoPage() {
           </p>
 
            <p className="mb-4 text-gray-700">
-            TEXT
+            Most students become paralyzed at the brainstorming stage because they feel pressured by what’s at stake. They only have a couple of essays to convey who they are, and whether or not those essays are “good” will determine the rest of their life!!! (or at least the next four years). And this makes sense. It’s easy to feel overwhelmed by the expectation that we show up as the most unique, impressive, or traumatized person in the room, and so we grasp for recognizable topics, scripts, or lessons. The result is personal statements that feel tired and overdone. Counterintuitively, the fastest way to find a compelling essay topic is often to stop looking for one. When students use brainstorming as a practice of exploring their experiences, interests, and values without worrying about admissions or appearing impressive, the more poignant essay topics tend to reveal themselves naturally.
           </p>
 
           
